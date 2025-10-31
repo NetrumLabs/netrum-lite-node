7af00cc077e8460d1187ca669fe170e7fffe74f3
Netrum Labs <netrumlabs@gmail.com>
2025-10-31 17:13:52 +0530

    Update system-cli.js

--- cli/system-cli.js
+++ cli/system-cli.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const filePath = path.join(__dirname, '../src/system/system/system.js');
+const filePath = path.join(__dirname, '../src/system/system/service-installer.js');
 
 // If user runs --help
 if (process.argv.includes('--help') || process.argv.includes('-h')) {
